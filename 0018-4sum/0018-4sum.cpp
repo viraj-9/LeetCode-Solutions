9cd1b2827016b1f4736915d3bc0e6820b1e69391
Viraj Rokade <165674628+viraj-9@users.noreply.github.com>
2026-08-03 01:14:58 +0530

    Time: 23 ms (53.87%), Space: 17.6 MB (13.1%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,27 +2,34 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> st;
-        
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
         for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1; j<n; j++){
-                set<long long> mp;
-                for(int k=j+1; k<n; k++){
-                    long long sum = nums[i]+nums[j];
-                    long long l = target-(sum+nums[k]);
-                    if(mp.find(l) != mp.end()){
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int) l};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target){
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++; l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
-                    else{
-                        mp.insert(nums[k]);
+                    else if(sum<target){
+                        k++;
+                    }else{
+                        l--;
                     }
                 }
             }
         }
-
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
