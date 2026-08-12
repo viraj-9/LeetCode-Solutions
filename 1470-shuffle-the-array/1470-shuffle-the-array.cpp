9f895ff9700e593d5b14e2cab446fa22ab823b7a
Viraj Rokade <165674628+viraj-9@users.noreply.github.com>
2026-08-13 00:49:54 +0530

    Time: 8 ms (17.36%), Space: 13.5 MB (19.91%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        int limit = nums.size();
-        int mid = limit/2;
-        for(int i=1; i<limit-1; i+=2){
-            nums.insert(nums.begin()+i, nums[mid]);
-            mid++;
-            nums.erase(nums.begin()+mid);
+        int limit=nums.size();
+        int mid=n;
+        vector<int> result;
+        int left=0;
+        for(int i=0; i<limit; i++){
+            if(i%2==0){
+                result.push_back(nums[left]);
+                left++;
+            }else{
+                result.push_back(nums[mid]);
+                mid++;
+            }
         }
-        return nums;
+        return result;
     }
 };
+// int limit = nums.size();
+//         int mid = limit/2;
+//         for(int i=1; i<limit-1; i+=2){
+//             nums.insert(nums.begin()+i, nums[mid]);
+//             mid++;
+//             nums.erase(nums.begin()+mid);
+//         }
+//         return nums;
