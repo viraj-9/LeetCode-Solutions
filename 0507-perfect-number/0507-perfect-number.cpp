8a6755e6298ee2ae52a0f97cd393291ea20101a4
Viraj Rokade <165674628+viraj-9@users.noreply.github.com>
2026-08-18 12:16:04 +0530

    Time: 1041 ms (48.4%), Space: 7.6 MB (89.13%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        int i=1;
+        while(i<=num/2){
+            if(num%i==0){
+                sum+=i;
+            }
+            i++;
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+    }
+};
