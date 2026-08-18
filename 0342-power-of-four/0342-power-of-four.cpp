7c8a029f0868dde02064980708d87d1a90cc00df
Viraj Rokade <165674628+viraj-9@users.noreply.github.com>
2026-08-18 13:43:56 +0530

    Time: 0 ms (100%), Space: 7.6 MB (99.05%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==1){
-            return true;
+        bool res = true;
+        if(n<=0){
+            return false;
         }
-        for(int i=0; i<sqrt(n); i++){
-            if(pow(4, i)==n){
-                return true;
+        while(n>1){
+            if(n%4!=0){
+                res=false;
             }
+            n/=4;
         }
-        return false;
+        return res;
     }
 };
+// if(n==1){
+//             return true;
+//         }
+//         for(int i=0; i<sqrt(n); i++){
+//             if(pow(4, i)==n){
+//                 return true;
+//             }
+//         }
