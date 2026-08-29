b69bf49ddd22b54d42a2bff38e6062d475c1e92a
Viraj Rokade <165674628+viraj-9@users.noreply.github.com>
2026-08-29 22:23:17 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int i=0;
-        bool result=false;
-        
-        if(n<0){
+        if(n<=0){
             return false;
         }
-        long long x=sqrt(n)+1;
-        while(i<=x){
-            if(n==(pow(2, i))){
-                result= true;
-            }
-            i++;
-        }
-        return result;
+
+        return (n & n-1)==0;
     }
 };
