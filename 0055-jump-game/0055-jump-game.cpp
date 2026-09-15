084efeaeece1fb47c7270ce0a9c342b3041cc719
Viraj Rokade <165674628+viraj-9@users.noreply.github.com>
2026-09-15 15:29:41 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,26 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n= nums.size();
-        if(n==1) return true;
-        if(nums[0]==0) return false;
-        int i;
-        int sum=0;
-        for(i=0; i<n-1; i++){
-            sum = nums[i]+i;
-            if(sum<=i){
-                return false;
-            }
-            if(sum>=n-1){
-                return true;
-            }
-            for(int j=i+1; j<=sum; j++){
-                if(nums[j]+j >= n-1){
-                    return true;
-                }
-                if(nums[j]+j > sum){
-                    sum = nums[j]+j;
-                }
-            }
+        int maxIndex = 0;
+
+        for(int i=0; i<n; i++){
+            if(i>maxIndex) return false;
+            maxIndex = max(maxIndex, i+nums[i]);
+            if(maxIndex >= n-1) return true;
         }
         return false;
     }
