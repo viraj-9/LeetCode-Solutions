59ba396fe7a2e92cc731d2e7dc41ed20d02f9527
Viraj Rokade <165674628+viraj-9@users.noreply.github.com>
2026-09-24 17:59:16 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.96%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,22 +3,8 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int left=0; 
         int right=nums.size()-1;
-        int n;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(left==right){
-                if(nums[left]<target) return left+1;
-                else return left;
-            }
-            if(target>nums[mid]){
-                left=mid+1;
-            }else if(target<nums[mid]){
-                right=mid-1;
-            }else{
-                return mid;
-            }
-            
-        }
-        return left;
+        int n = nums.size();
+        int index = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return index;
     }
 };
